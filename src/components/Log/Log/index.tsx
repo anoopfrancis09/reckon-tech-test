c7d555cab4a8e53e10f1932aa5f9a2bb66e99d8b
anoopfrancis09 <64771040+anoopfrancis09@users.noreply.github.com>
2022-06-14 17:26:19 +1000

    Add files via upload

---
+++ src/components/Log/Log/index.tsx
@@ -0,0 +1,48 @@
+import Button from '@mui/material/Button';
+import { ILogDataProps, LogData } from '../../types';
+import './style.css'
+
+function LogPage(props: ILogDataProps) {
+    const { logsData, isPaused, hanldePauseLogs } = props
+
+    const generateLogsView = (eachLog: LogData) => {
+        return (
+            <>
+                <h5>{`Updates for ${eachLog.time} `}</h5>
+                {
+                    eachLog.logs.map((stockDataLog: string, index) => {
+                        return (
+                            <span key={`log-row-${index}`}>
+                                {stockDataLog}
+                            </span>
+                        )
+                    })
+                }
+            </>
+        )
+    }
+
+
+    return (
+        <div className='logsContainer'>
+            <div className='titleContainer'>
+                <h2>Log</h2>
+                <Button variant="outlined" style={{ height: 30 }} onClick={hanldePauseLogs}>{isPaused ? 'Resume' : 'Pause'}</Button>
+            </div>
+            {logsData.length ?
+                <div className='logsSection'>
+                    {
+                        logsData.map((eachLog, index) => {
+                            return (
+                                <div key={`log-section-${index}`} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                                    {generateLogsView(eachLog)}
+                                </div>
+                            )
+                        })
+                    }
+                </div> : null}
+        </div >
+    );
+}
+
+export default LogPage;
